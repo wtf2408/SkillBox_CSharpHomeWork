59f8fa08bb01852544ed85f5a276cc497f34f3ef
wtf2408 <bogdanevseyv@yandex.ru>
2023-02-21 17:24:33 +0300

    дз модуль 4

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,65 @@
+﻿#region Задание 1. Случайная матрица
+Console.WriteLine("Задание 1. Случайная матрица\nВведите количество строк матрицы");
+int rowMatrix = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов матрицы");
+int columnMatrix = int.Parse(Console.ReadLine());
+
+int[,] matrix = CreateMatrix(rowMatrix, columnMatrix);
+PrintMatrix(matrix);
+
+int sum = 0;
+foreach (var item in matrix)
+{
+    sum += item;
+}
+Console.WriteLine($"сумма - {sum}\n");
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] CreateMatrix(int row, int column) 
+{
+    int[,] matrix = new int[row, column];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(1, 20);
+        }
+    }
+    return matrix;
+}
+#endregion
+
+#region Задание 2. Сложение матриц
+Console.WriteLine("Задание 2. Сложение матриц");
+int[,] matrix1 = CreateMatrix(3, 3);
+int[,] matrix2 = CreateMatrix(3, 3);
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+int[,] sumMatrix = SumMatrixElements(matrix1, matrix2);
+PrintMatrix(sumMatrix);
+int[,] SumMatrixElements(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrix3 = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < matrix3.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3.GetLength(1); j++)
+        {
+            matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+        }
+    }
+    return matrix3;
+}
+#endregion
--- SkillBox_CSharpHomeWork/Program.cs
+++ SkillBox_CSharpHomeWork/Program.cs
@@ -1,39 +1,62 @@
 ﻿#region Тернарный оператор
-Random random = new Random();
-int a = random.Next(1, 10);
-Console.WriteLine(a);
-Console.WriteLine((a > 5) ? "больше 5":"меньше 5");
+//Random random = new Random();
+//int a = random.Next(1, 10);
+//Console.WriteLine(a);
+//Console.WriteLine((a > 5) ? "больше 5":"меньше 5");
 #endregion
 
 #region Switch-case
-int day = Convert.ToInt32(Console.ReadLine());
-switch (day)
-{
-	case 1:
-		Console.WriteLine("Пн");
-		break;
-    case 2:
-        Console.WriteLine("Вт");
-        break;
-    case 3:
-        Console.WriteLine("Ср");
-        break;
-    case 4:
-        Console.WriteLine("Чт");
-        break;
-    case 5:
-        Console.WriteLine("Пт");
-        break;
-    case 6:
-        Console.WriteLine("Сб");
-        break;
-    case 7:
-        Console.WriteLine("Вс");
-        break;
-    default:
-        Console.WriteLine("Нет такого дня");
-        break;
-}
+//int day = Convert.ToInt32(Console.ReadLine());
+//switch (day)
+//{
+//	case 1:
+//		Console.WriteLine("Пн");
+//		break;
+//    case 2:
+//        Console.WriteLine("Вт");
+//        break;
+//    case 3:
+//        Console.WriteLine("Ср");
+//        break;
+//    case 4:
+//        Console.WriteLine("Чт");
+//        break;
+//    case 5:
+//        Console.WriteLine("Пт");
+//        break;
+//    case 6:
+//        Console.WriteLine("Сб");
+//        break;
+//    case 7:
+//        Console.WriteLine("Вс");
+//        break;
+//    default:
+//        Console.WriteLine("Нет такого дня");
+//        break;
+//}
 #endregion
 
-
+#region Зубчатый массив
+//int[][] arr = new int[3][];
+//arr[0] = new int[2];
+//arr[0][0] = 1;
+//arr[0][1] = 2;
+//arr[1] = new int[] { 1, 2, 3 };
+//arr[2] = new int[] { 1, 2, 3, 4 };
+//for (int i = 0; i < arr.Length; i++)
+//{
+//	for (int j = 0; j < arr[i].Length; j++)
+//	{
+//		Console.Write($"{arr[i][j]} ");
+//	}
+//	Console.WriteLine();
+//}
+//foreach (var array in arr)
+//{
+//	foreach (var item in array)
+//	{
+//		Console.Write($"{item} ");
+//	}
+//	Console.WriteLine();
+//}
+#endregion
