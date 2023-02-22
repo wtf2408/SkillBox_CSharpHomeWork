4ffc1fab66c74b0f64a1c1c0d6df75b4c5c464c1
wtf2408 <bogdanevseyv@yandex.ru>
2023-02-22 19:13:43 +0300

    дз модуль 5

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задание 1.Метод разделения строки на слова
+Console.WriteLine("Введите предложение");
+string sentence = Console.ReadLine();
+string[] words = SplitSentence(sentence);
+PrintText(words);
+string[] SplitSentence(string sentence)
+{
+    string[] words = sentence.Split(" ");
+    return words;
+}
+void PrintText(string[] text)
+{
+    foreach (var item in text)
+    {
+        Console.WriteLine(item);
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] SplitSentence(string sentence)
+{
+    string[] words = sentence.Split(" ");
+    return words;
+}
+string[] ReverseText(string text)
+{
+    string[] words = SplitSentence(text);
+    int count = words.Length - 1;
+    string[] wordsReverse = new string[words.Length];
+    for (int i = 0; i < words.Length; i++)
+    {
+        wordsReverse[i] = words[count];
+        count--;
+    }
+    return wordsReverse;
+}
+
+Console.WriteLine("Введите предложение");
+string text = Console.ReadLine();
+
+string[] wordsReverse = ReverseText(text);
+foreach (var item in wordsReverse)
+{
+    Console.Write($"{item} ");
+}
